99dc97b5182f701ceda38cd9291a0bd53358230b
sun129129 <sunstudy129@gmail.com>
2025-11-19 17:51:58 +0900

    feat: update README and user_supplement model

--- server/app/db/models/user_supplement.py
+++ server/app/db/models/user_supplement.py
@@ -3,7 +3,7 @@ from sqlalchemy import (
 )
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from app.db.base_class import Base
+from app.core.database import Base
 
 class UserSupplement(Base):
     __tablename__ = "user_supplement"
