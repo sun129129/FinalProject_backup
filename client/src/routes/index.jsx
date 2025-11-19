999390cfb6a4fe3669f3efcd7cd9cc678ac4ed6a
sun129129 <sunstudy129@gmail.com>
2025-11-19 12:44:24 +0900

    feat: user_supplement db 변경

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -18,10 +18,7 @@ import ResetPassword from '../features/auth/ResetPassword';
 // Dashboard 및 Survey
 import Dashboard from '../features/dashboard/Dashboard';
 import Survey from '../features/survey/Survey';
-<<<<<<< Updated upstream
-=======
 import HealthResult from '../features/survey/HealthResult';
->>>>>>> Stashed changes
 import SupplementOCR from '../features/intake/SupplementOCR'; // OCR 컴포넌트 추가
 import MySupplements from '../features/intake/MySupplements.jsx'; // 내 영양제 페이지
 
@@ -50,10 +47,7 @@ const AppRoutes = () => {
            로그인 여부를 체크하는 로직이 추가되어야 합니다. */}
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/survey" element={<Survey />} />
-<<<<<<< Updated upstream
-=======
       <Route path="/survey/result" element={<HealthResult />} />
->>>>>>> Stashed changes
       <Route path="/intake-ocr" element={<SupplementOCR />} /> {/* OCR 페이지 라우트 추가 */}
       <Route path="/my-supplements" element={<MySupplements />} /> {/* 내 영양제 페이지 라우트 추가 */}
 
