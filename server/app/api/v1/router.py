c1ad668aee9a3020802ef31d11bf90c5c15bc59c
sun129129 <sunstudy129@gmail.com>
2025-11-20 15:29:41 +0900

    feat: 회원가입 페이지 수정완료 db 수정

--- server/app/api/v1/router.py
+++ server/app/api/v1/router.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter
 from app.api.v1.endpoints import auth, survey, ocr, user_supplement
-from app.api.v1.endpoints import users
 
 api_router = APIRouter()
 
 # 2. 회원가입 라우터 등록 (주소: /api/v1/users/signup)
-api_router.include_router(users.router, prefix="/users", tags=["Users"])
 api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
 api_router.include_router(survey.router, prefix="/survey", tags=["Survey"])
 api_router.include_router(ocr.router, prefix="/ocr", tags=["OCR"])
